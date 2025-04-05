bad084ccdff0eb84b71da3f7d129adb7218e8b5d
nwpm <fwxgkt@gmail.com>
2025-04-05 16:23:16 +0300

    Add header guards, add declarations of sml_sqrt, sml_decimal_pow, sml_quadrtc_equation and struct equation_solution

--- algebra/algebra.h
+++ algebra/algebra.h
@@ -1,3 +1,6 @@
+#ifndef ALGEBRA_H
+#define ALGEBRA_H
+
 /*Find absolute value of number*/
 float sml_absolute_value(float val);
 
@@ -6,3 +9,17 @@ float sml_sqrt(float val);
 
 /*sml_pow for now find only integral exponents*/
 float sml_pow(float val, int exp);
+
+typedef struct equation_solution {
+  float first_root = 0.f;
+  float second_root = 0.f;
+  int has_real_solution = 1;
+} equation_solution;
+
+/*Calculate quadratic equation*/
+equation_solution sml_quadrtc_equation(float a, float b, float c);
+
+/*Square root*/
+float sml_decimal_pow(float num, float pow);
+
+#endif
