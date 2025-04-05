e58e26467ec5cb17560d5c2b498becb2b47ac7f9
nwpm <fwxgkt@gmail.com>
2025-04-05 16:24:18 +0300

    Add separate file for all constants

---
+++ constants/constnts.h
@@ -0,0 +1,10 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double PI = 3.141592;
+const float STRGHT_ANG = 180.f;
+const float FULL_TRN_DEG = 360.f;
+const int FULL_TRN_INT_DEG = 360;
+const double FULL_TRN_RAD = (2 * PI);
+
+#endif // !CONSTANTS_H
