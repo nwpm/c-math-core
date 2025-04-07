f0c8d1fbebc1b5fcd1c6eee92d695f5874e052f4
nwpm <fwxgkt@gmail.com>
2025-04-07 18:51:45 +0300

    Rename constants

--- constants/constnts.h
+++ constants/constnts.h
@@ -2,10 +2,9 @@
 #define CONSTANTS_H
 
 const double PI = 3.141592;
-const double QUARTER_TRN_RAD = PI / 2.f;
-const double HALF_TRN_RAD = PI;
-const double ONE_AND_HALF_TRN_RAD = (3.f * PI) / 2.f;
-const double FULL_TRN_RAD = (2 * PI);
+const double HALF_PI = (PI / 2.f);
+const double THREE_PI_OVER_TWO = ((3.f * PI) / 2.f);
+const double TWO_PI = (2 * PI);
 
 const float STRGHT_ANG = 180.f;
 const float FULL_TRN_DEG = 360.f;
