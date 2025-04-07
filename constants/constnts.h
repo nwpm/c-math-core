40eb0f7179f663d1350714f82497ffb21fc5eb1f
nwpm <fwxgkt@gmail.com>
2025-04-07 16:50:23 +0300

    #5 Add new constants for trignm

--- constants/constnts.h
+++ constants/constnts.h
@@ -2,9 +2,13 @@
 #define CONSTANTS_H
 
 const double PI = 3.141592;
+const double QUARTER_TRN_RAD = PI / 2.f;
+const double HALF_TRN_RAD = PI;
+const double ONE_AND_HALF_TRN_RAD = (3.f * PI) / 2.f;
+const double FULL_TRN_RAD = (2 * PI);
+
 const float STRGHT_ANG = 180.f;
 const float FULL_TRN_DEG = 360.f;
 const int FULL_TRN_INT_DEG = 360;
-const double FULL_TRN_RAD = (2 * PI);
 
 #endif // !CONSTANTS_H
