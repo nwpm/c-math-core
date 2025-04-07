e12c52e11580703673d009682b006ba647d87c2b
nwpm <fwxgkt@gmail.com>
2025-04-07 16:47:53 +0300

    #5 Remove main

--- euclide_geom/euclide_geom.c
+++ euclide_geom/euclide_geom.c
@@ -1,6 +1,5 @@
 #include "euclide_geom.h"
 #include "../constants/constnts.h"
-#include <stdio.h>
 
 float sml_circumference(float rad) {
   if (rad < 0) {
@@ -9,10 +8,4 @@ float sml_circumference(float rad) {
 
   float result = 2 * PI * rad;
   return result;
-}
-
-int main(void) {
-
-  printf("%f\n", sml_circumference(2.15f));
-  return 0;
 }
