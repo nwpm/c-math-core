1bcf160acc2a76e7ed3be7964e0621a581bae970
nwpm <fwxgkt@gmail.com>
2025-08-30 17:56:48 +0300

    feat(circle): add area and circumference

--- src/circle/cm_circle.h
+++ src/circle/cm_circle.h
@@ -1,8 +1,11 @@
 #ifndef CM_CIRCLE_H
 #define CM_CIRCLE_H
 
-#include <stdbool.h>
+#define PI 3.14159265
 
-int on_circle(double x, double y, double r, double px1, double px2);
+int cm_on_circle(double x, double y, double r, double px1, double px2);
+
+inline double cm_circumference(double r) { return 2 * PI * r; }
+inline double cm_circle_area(double r) { return PI * r * r; }
 
 #endif
