5a2e5870371c164941ced0be019d9bec0e6adf5d
nwpm <fwxgkt@gmail.com>
2025-09-26 20:07:00 +0300

    test(matrix): add test for 'create_from_matrix'

--- test/matrix/test_matrix.c
+++ test/matrix/test_matrix.c
@@ -1,14 +1,60 @@
-#include "../../src/matrix/cm_matrix_int.h"
+#include "../../src/matrix/cm_matrix_double.h"
+#include "../Unity/src/unity.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdio.h>
 
-int main(){
+void setUp() {}
+void tearDown() {}
 
-  CmMatrixInt* matrix = cm_matrix_int_alloc(4, 5);
+// Create from matrix
 
-  cm_matrix_int_set_identity(matrix);
+void check_create_from_matrix(size_t orig_rows, size_t orig_cols,
+                              double init_val) {
 
-  _cm_matrix_int_printf(matrix);
+  CmMatrixDouble *source = cm_matrix_double_alloc(orig_rows, orig_cols);
+  TEST_ASSERT_NOT_NULL(source);
 
-  cm_matrix_int_free(matrix);
+  cm_matrix_double_set_all(source, init_val);
 
-  return 0;
+  CmMatrixDouble *copy = cm_matrix_double_create_from_matrix(source);
+  TEST_ASSERT_NOT_NULL(copy);
+
+  TEST_ASSERT_TRUE(cm_matrix_double_is_equal(source, copy));
+
+  cm_matrix_double_free(source);
+  cm_matrix_double_free(copy);
+}
+
+void test_create_from_matrix_rows_1_cols_1(){
+  check_create_from_matrix(1, 1, 5.);
+}
+
+void test_create_from_matrix_rows_1_cols_5(){
+  check_create_from_matrix(1, 5, 5.);
+}
+
+void test_create_from_matrix_rows_5_cols_1(){
+  check_create_from_matrix(5, 1, 5.);
+}
+
+void test_create_from_matrix_rows_100_cols_100(){
+  check_create_from_matrix(100, 100, 5.);
+}
+
+// Set Identity
+
+
+
+int main() {
+
+  UNITY_BEGIN();
+
+  puts("\nCreate from matrix\n");
+  RUN_TEST(test_create_from_matrix_rows_1_cols_1);
+  RUN_TEST(test_create_from_matrix_rows_1_cols_5);
+  RUN_TEST(test_create_from_matrix_rows_5_cols_1);
+  RUN_TEST(test_create_from_matrix_rows_100_cols_100);
+
+  return UNITY_END();
 }
