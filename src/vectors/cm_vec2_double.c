63a13c51c76b4149123a6f2a00fca58b0e073384
nwpm <fwxgkt@gmail.com>
2025-10-05 20:01:42 +0300

    feat(vec2): add 'vec2' core logic
    - Add allocation functions
    - Add operation functions
    - Add getters/setters

---
+++ src/vectors/cm_vec2_double.c
@@ -0,0 +1,66 @@
+#include "cm_vec2_double.h"
+#include <math.h>
+#include <stdlib.h>
+
+CmVec2Double *cm_vec2_double_alloc() {
+
+  CmVec2Double *vec = malloc(sizeof(CmVec2Double));
+  if (!vec)
+    return NULL;
+
+  return vec;
+}
+
+CmVec2Double *cm_vec2_double_init(double x, double y) {
+
+  CmVec2Double *vec = cm_vec2_double_alloc();
+  if (!vec)
+    return NULL;
+
+  vec->x = x;
+  vec->y = y;
+
+  return vec;
+}
+
+CmVec2Double *cm_vec2_double_create_from_vec(CmVec2Double *orig_vec) {
+
+  CmVec2Double *vec = cm_vec2_double_alloc();
+  if (!vec)
+    return NULL;
+
+  vec->x = orig_vec->x;
+  vec->y = orig_vec->y;
+
+  return vec;
+}
+
+void cm_vec2_double_free(CmVec2Double *vec){
+  free(vec);
+}
+
+void cm_vec2_double_sum(CmVec2Double *vec_a, const CmVec2Double *vec_b) {
+  vec_a->x += vec_b->x;
+  vec_a->y += vec_b->y;
+}
+
+void cm_vec2_double_sub(CmVec2Double *vec_a, const CmVec2Double *vec_b) {
+  vec_a->x -= vec_b->x;
+  vec_a->y -= vec_b->y;
+}
+
+void cm_vec2_double_scale(CmVec2Double *vec, double scale) {
+  vec->x *= scale;
+  vec->y *= scale;
+}
+
+double cm_vec2_double_norm(const CmVec2Double *vec) {
+  return sqrt(vec->x * vec->x + vec->y * vec->y);
+}
+
+double cm_vec2_double_dot(const CmVec2Double *vec_a,
+                          const CmVec2Double *vec_b) {
+  return vec_a->x * vec_b->x + vec_a->y * vec_b->y;
+}
+
+CmVec2Double *cm_vec2_double_normalize(const CmVec2Double *vec);
--- src/vectors/cm_vec2_double.h
+++ src/vectors/cm_vec2_double.h
@@ -13,17 +13,45 @@ typedef enum CmVec2Code {
 
 } CmMatrixCode;
 
+// TODO: Hide struct
 typedef struct CmVec2Double {
   double x;
   double y;
-  double z;
 } CmVec2Double;
 
-void cm_Vec2_double_sum(const CmVec2Double *vec_a, const CmVec2Double *vec_b);
-void cm_Vec2_double_sub(const CmVec2Double *vec_a, const CmVec2Double *vec_b);
-void cm_Vec2_double_scale(CmVec2Double *vec, double scale);
-double cm_Vec2_double_norm(const CmVec2Double *vec);
-double cm_Vec2_double_dot(const CmVec2Double *vec_a, const CmVec2Double *vec_b);
-CmVec2Double *cm_Vec2_double_normalize(const CmVec2Double *vec);
+/* Allocation */
+CmVec2Double *cm_vec2_double_alloc();
+CmVec2Double *cm_vec2_double_init(double x, double y);
+CmVec2Double *cm_vec2_double_create_from_vec(CmVec2Double *orig_vec);
+
+void cm_vec2_double_free(CmVec2Double *vec);
+
+/* Operations */
+void cm_vec2_double_sum(CmVec2Double *vec_a, const CmVec2Double *vec_b);
+void cm_vec2_double_sub(CmVec2Double *vec_a, const CmVec2Double *vec_b);
+void cm_vec2_double_scale(CmVec2Double *vec, double scale);
+double cm_vec2_double_norm(const CmVec2Double *vec);
+double cm_vec2_double_dot(const CmVec2Double *vec_a, const CmVec2Double *vec_b);
+
+/* Normalize */
+CmVec2Double *cm_vec2_double_normalize(const CmVec2Double *vec);
+
+/* Setter */
+static inline void cm_vec2_double_set_x(CmVec2Double *vec, double x_val){
+  vec->x = x_val;
+}
+
+static inline void cm_vec2_double_set_y(CmVec2Double *vec, double y_val){
+  vec->x = y_val;
+}
+
+/* Getter */
+static inline double cm_vec2_double_get_x(CmVec2Double *vec){
+  return vec->x;
+}
+
+static inline double cm_vec2_double_get_y(CmVec2Double *vec){
+  return vec->y;
+}
 
 #endif
