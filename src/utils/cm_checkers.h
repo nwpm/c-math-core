9684897dc94dfa26f96125122943ba91eefb1eba
nwpm <fwxgkt@gmail.com>
2025-10-17 20:36:27 +0300

    feat(utils): add 'alloc_null' checker

--- src/utils/cm_checkers.h
+++ src/utils/cm_checkers.h
@@ -10,25 +10,39 @@
     }                                                                          \
   } while (0)
 
-#define CM_SQUARE_CHECK(matrix)                                                \
+#define CM_ALLOC_CHECK_NULL(ptr)                                               \
   do {                                                                         \
-    if ((matrix->rows) != (matrix->columns)) {                                 \
-      return CM_ERR_MATRIX_NOT_SQUARE;                                         \
+    if ((ptr) == NULL) {                                                       \
+      return CM_ERR_ALLOC_FAILED;                                              \
     }                                                                          \
   } while (0)
 
-#define CM_SIZE_MATCH(matrix_a, matrix_b)                                      \
+#define CM_MATRIX_SQUARE_CHECK(matrix)                                         \
+  do {                                                                         \
+    if ((matrix->rows) != (matrix->columns)) {                                 \
+      return CM_MATRIX_ERR_NOT_SQUARE;                                         \
+    }                                                                          \
+  } while (0)
+
+#define CM_MATRIX_SIZE_MATCH(matrix_a, matrix_b)                               \
   do {                                                                         \
     if (((matrix_a->rows) != (matrix_b->rows)) ||                              \
         ((matrix_a->columns) != (matrix_b->columns))) {                        \
-      return CM_ERR_SIZE_MISMATCH;                                             \
+      return CM_MATRIX_ERR_SIZE_MISMATCH;                                      \
     }                                                                          \
   } while (0)
 
-#define CM_MULT_SIZE_MATCH(matrix_a, matrix_b)                                 \
+#define CM_MATRIX_MULT_SIZE_MATCH(matrix_a, matrix_b)                          \
   do {                                                                         \
     if (((matrix_a->columns) != (matrix_b->rows))) {                           \
-      return CM_ERR_MULT_SIZE_MISMATCH;                                        \
+      return CM_MATRIX_ERR_MULT_SIZE_MISMATCH;                                 \
+    }                                                                          \
+  } while (0)
+
+#define CM_MATRIX_BUFF_NULL_CHECK(matrix)                                      \
+  do {                                                                         \
+    if (((matrix->data) == (NULL))) {                                          \
+      return CM_ERR_NULL_POINTER;                                              \
     }                                                                          \
   } while (0)
 
