8f88a6fdb3a3147d8db3249c871bcd6b0b72a3fa
nwpm <fwxgkt@gmail.com>
2025-12-04 20:51:26 +0300

    feat(mat2): add 'inv', 'trace' and 'det' funcs

--- src/matrix/cm_matrix2.c
+++ src/matrix/cm_matrix2.c
@@ -1,58 +1,73 @@
 #include "cm_matrix2.h"
 
-void cm_mat2_make(float *src, cm_matrix2 dest){
+void cm_mat2_make(float *src, cm_matrix2 dest) {
   dest.data[0] = src[0];
   dest.data[1] = src[1];
   dest.data[2] = src[2];
   dest.data[3] = src[3];
 }
 
-void cm_mat2_copy(cm_matrix2 mat, cm_matrix2 dest){
+void cm_mat2_copy(cm_matrix2 mat, cm_matrix2 dest) {
   dest.data[0] = mat.data[0];
   dest.data[1] = mat.data[1];
   dest.data[2] = mat.data[2];
   dest.data[3] = mat.data[3];
 }
 
-void cm_mat2_identity(cm_matrix2 mat){
+void cm_mat2_identity(cm_matrix2 mat) {
   mat.data[0] = 0.f;
   mat.data[1] = 1.f;
   mat.data[2] = 0.f;
   mat.data[3] = 1.f;
 }
 
-void cm_mat2_zero(cm_matrix2 mat){
+void cm_mat2_zero(cm_matrix2 mat) {
   mat.data[0] = 0.f;
   mat.data[1] = 1.f;
   mat.data[2] = 0.f;
   mat.data[3] = 1.f;
 }
 
-void cm_mat2_mul(cm_matrix2 m1, cm_matrix2 m2, cm_matrix2 dest){
+void cm_mat2_mul(cm_matrix2 m1, cm_matrix2 m2, cm_matrix2 dest) {
   dest.data[0] = m1.data[0] * m2.data[0] + m1.data[1] * m2.data[2];
   dest.data[1] = m1.data[0] * m2.data[1] + m1.data[1] * m2.data[3];
   dest.data[2] = m1.data[2] * m2.data[0] + m1.data[3] * m2.data[2];
   dest.data[3] = m1.data[2] * m2.data[1] + m1.data[3] * m2.data[3];
 }
 
-void cm_mat2_transpose_to(cm_matrix2 mat, cm_matrix2 dest){
+void cm_mat2_transpose_to(cm_matrix2 mat, cm_matrix2 dest) {
   dest.data[0] = mat.data[0];
   dest.data[1] = mat.data[2];
   dest.data[2] = mat.data[1];
   dest.data[3] = mat.data[3];
 }
 
-void cm_mat2_transpose(cm_matrix2 mat){
+void cm_mat2_transpose(cm_matrix2 mat) {
   float tmp = mat.data[1];
   mat.data[1] = mat.data[2];
   mat.data[2] = tmp;
 }
 
-void mat2_scale(cm_matrix2 mat, float s){
+void cm_mat2_scale(cm_matrix2 mat, float s) {
   mat.data[0] *= s;
   mat.data[1] *= s;
   mat.data[2] *= s;
   mat.data[3] *= s;
 }
 
+void cm_mat2_inv(cm_matrix2 mat, cm_matrix2 dest){
+  float s = 1.f / cm_mat2_det(mat);
+  dest.data[0] = mat.data[3];
+  dest.data[1] = -mat.data[1];
+  dest.data[2] = -mat.data[2];
+  dest.data[3] = mat.data[1];
+  cm_mat2_scale(dest, s);
+}
 
+float cm_mat2_det(cm_matrix2 mat) {
+  return mat.data[0] * mat.data[3] - mat.data[2] * mat.data[1];
+}
+
+float cm_mat2_trace(cm_matrix2 mat){
+  return mat.data[0] + mat.data[3];
+}
--- src/matrix/cm_matrix2.h
+++ src/matrix/cm_matrix2.h
@@ -7,20 +7,17 @@
 /* TODO:
    void  mat2_identity_array(mat2 * restrict mats, size_t count)
    void  mat2_mulv(mat2 m, vec2 v, vec2 dest)
-   void  mat2_inv(mat2 mat, mat2 dest)
    void  mat2_swap_col(mat2 mat, int col1, int col2)
    void  mat2_swap_row(mat2 mat, int row1, int row2)
-   float mat2_det(mat2 m)
-   float mat2_trace(mat2 m)
    float mat2_rmc(vec2 r, mat2 m, vec2 c)
 */
 
 /*
-  * Matrix data position
-  * | data[0] data[1]|
-  * | data[2] data[3]|
-  *
-*/
+ * Matrix data position
+ * | data[0] data[1]|
+ * | data[2] data[3]|
+ *
+ */
 typedef struct cm_matrix2 {
   float data[4];
 } cm_matrix2;
@@ -33,6 +30,10 @@ void cm_mat2_zero(cm_matrix2 mat);
 void cm_mat2_mul(cm_matrix2 m1, cm_matrix2 m2, cm_matrix2 dest);
 void cm_mat2_transpose_to(cm_matrix2 mat, cm_matrix2 dest);
 void cm_mat2_transpose(cm_matrix2 mat);
-void mat2_scale(cm_matrix2 mat, float s);
+void cm_mat2_scale(cm_matrix2 mat, float s);
 
+void cm_mat2_inv(cm_matrix2 mat, cm_matrix2 dest);
+
+float cm_mat2_det(cm_matrix2 mat);
+float cm_mat2_trace(cm_matrix2 mat);
 #endif
