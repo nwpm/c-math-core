5e8e5027a706369ef274285cea6988b0987d4e14
nwpm <fwxgkt@gmail.com>
2025-12-04 20:29:54 +0300

    feat(mat2): add new funcs, change matrix data storage
    Added:
    - `cm_mat2_mul`
    - `cm_mat2_transpose`
    - `cm_mat2_transpose_to`
    - `cm_mat2_scale`

--- src/matrix/cm_matrix2.h
+++ src/matrix/cm_matrix2.h
@@ -6,11 +6,7 @@
 
 /* TODO:
    void  mat2_identity_array(mat2 * restrict mats, size_t count)
-   void  mat2_mul(mat2 m1, mat2 m2, mat2 dest)
    void  mat2_mulv(mat2 m, vec2 v, vec2 dest)
-   void  mat2_transpose_to(mat2 mat, mat2 dest)
-   void  mat2_transpose(mat2 m)
-   void  mat2_scale(mat2 m, float s)
    void  mat2_inv(mat2 mat, mat2 dest)
    void  mat2_swap_col(mat2 mat, int col1, int col2)
    void  mat2_swap_row(mat2 mat, int row1, int row2)
@@ -19,16 +15,24 @@
    float mat2_rmc(vec2 r, mat2 m, vec2 c)
 */
 
+/*
+  * Matrix data position
+  * | data[0] data[1]|
+  * | data[2] data[3]|
+  *
+*/
 typedef struct cm_matrix2 {
-
-  float m00, m01;
-  float m10, m11;
-
+  float data[4];
 } cm_matrix2;
 
 void cm_mat2_make(float *src, cm_matrix2 dest);
 void cm_mat2_copy(cm_matrix2 mat, cm_matrix2 dest);
 void cm_mat2_identity(cm_matrix2 mat);
 void cm_mat2_zero(cm_matrix2 mat);
+
+void cm_mat2_mul(cm_matrix2 m1, cm_matrix2 m2, cm_matrix2 dest);
+void cm_mat2_transpose_to(cm_matrix2 mat, cm_matrix2 dest);
+void cm_mat2_transpose(cm_matrix2 mat);
+void mat2_scale(cm_matrix2 mat, float s);
 
 #endif
