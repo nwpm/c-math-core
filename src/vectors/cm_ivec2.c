c87dbce615d07f2db769be9d721d99a96368c1b7
nwpm <fwxgkt@gmail.com>
2025-12-05 19:34:57 +0300

    feat(ivec2): add 'scale', 'dot', 'fill', 'cross'

--- src/vectors/cm_ivec2.c
+++ src/vectors/cm_ivec2.c
@@ -20,4 +20,25 @@ void cm_ivec2_sub(cm_ivec2 a, cm_ivec2 b, cm_ivec2 dest){
   dest.y = a.x - b.y;
 }
 
+void cm_ivec2_scale(cm_ivec2 v, int s, cm_ivec2 dest){
+  dest.x = v.x * s;
+  dest.y = v.y * s;
+}
+
+void cm_ivec2_fill(cm_ivec2 v, int val){
+  v.x = val;
+  v.y = val;
+}
+
+int cm_ivec2_dot(cm_ivec2 a, cm_ivec2 b){
+  return a.x * b.x + a.y * b.y;
+}
+
+int cm_ivec2_cross(cm_ivec2 a, cm_ivec2 b){
+  return a.x * b.y - a.y * b.x;
+}
+
+
+
+
 
--- src/vectors/cm_ivec2.h
+++ src/vectors/cm_ivec2.h
@@ -14,4 +14,10 @@ void cm_ivec2_zero(cm_ivec2 v);
 void cm_ivec2_add(cm_ivec2 a, cm_ivec2 b, cm_ivec2 dest);
 void cm_ivec2_sub(cm_ivec2 a, cm_ivec2 b, cm_ivec2 dest);
 
+void cm_ivec2_scale(cm_ivec2 v, int s, cm_ivec2 dest);
+void cm_ivec2_fill(cm_ivec2 v, int val);
+
+int cm_ivec2_dot(cm_ivec2 a, cm_ivec2 b);
+int cm_ivec2_cross(cm_ivec2 a, cm_ivec2 b);
+
 #endif
