2269c5851c453840b09835566398294b8cf02721
nwpm <fwxgkt@gmail.com>
2026-02-15 10:32:44 +0300

    refactor: ivec2 global refactor
    -Make structure public
    -Add const modificator for params
    -Optimize 'ivec2_abs'

--- src/ivector/ivec2.c
+++ src/ivector/ivec2.c
@@ -1,65 +1,72 @@
-#include "../utils/cm_utils.h"
-#include "../../../include/cm_core/cm_ivec2.h"
-#include "cm_ivec2_internal.h"
+#include "ivector2.h"
 
 // For sqrt
 #include <math.h>
 
-void cm_ivec2_copy(cm_ivec2_t v, cm_ivec2_t dest) {
-  dest.x = v.x;
-  dest.y = v.y;
+void cm_ivec2_init(cm_ivec2_t *v, int64_t x, int64_t y) {
+  v->x = x;
+  v->y = y;
 }
 
-void cm_ivec2_zero(cm_ivec2_t v) {
-  v.x = 0;
-  v.y = 0;
+void cm_ivec2_copy(const cm_ivec2_t v, cm_ivec2_t *dest) {
+  dest->x = v.x;
+  dest->y = v.y;
 }
 
-void cm_ivec2_add(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest) {
-  dest.x = a.x + b.x;
-  dest.y = a.y + b.y;
+void cm_ivec2_zero(cm_ivec2_t *v) {
+  v->x = 0;
+  v->y = 0;
 }
 
-void cm_ivec2_sub(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest) {
-  dest.x = a.x - b.y;
-  dest.y = a.x - b.y;
+void cm_ivec2_add(const cm_ivec2_t a, const cm_ivec2_t b, cm_ivec2_t *dest) {
+  dest->x = a.x + b.x;
+  dest->y = a.y + b.y;
 }
 
-void cm_ivec2_scale(cm_ivec2_t v, int s, cm_ivec2_t dest) {
-  dest.x = v.x * s;
-  dest.y = v.y * s;
+void cm_ivec2_sub(const cm_ivec2_t a, const cm_ivec2_t b, cm_ivec2_t *dest) {
+  dest->x = a.x - b.y;
+  dest->y = a.x - b.y;
 }
 
-void cm_ivec2_fill(cm_ivec2_t v, int val) {
-  v.x = val;
-  v.y = val;
+void cm_ivec2_scale(const cm_ivec2_t v, int64_t s, cm_ivec2_t *dest) {
+  dest->x = v.x * s;
+  dest->y = v.y * s;
 }
 
-void cm_ivec2_abs(cm_ivec2_t v, cm_ivec2_t dest) {
-  dest.x = _cm_abs(v.x);
-  dest.y = _cm_abs(v.y);
+void cm_ivec2_fill(int64_t val, cm_ivec2_t *dest) {
+  dest->x = val;
+  dest->y = val;
 }
 
-void cm_ivec2_max(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest) {
-  dest.x = (a.x >= b.x) ? a.x : b.x;
-  dest.y = (a.y >= b.y) ? a.y : b.y;
+void cm_ivec2_abs(const cm_ivec2_t v, cm_ivec2_t *dest) {
+  dest->x = ~INT64_MIN & v.x;
+  dest->y = ~INT64_MIN & v.y;
 }
 
-void cm_ivec2_min(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest) {
-  dest.x = (a.x >= b.x) ? b.x : a.x;
-  dest.y = (a.y >= b.y) ? b.y : a.y;
+void cm_ivec2_max(const cm_ivec2_t a, const cm_ivec2_t b, cm_ivec2_t *dest) {
+  dest->x = (a.x >= b.x) ? a.x : b.x;
+  dest->y = (a.y >= b.y) ? a.y : b.y;
 }
 
-int cm_ivec2_dot(cm_ivec2_t a, cm_ivec2_t b) { return a.x * b.x + a.y * b.y; }
+void cm_ivec2_min(const cm_ivec2_t a, const cm_ivec2_t b, cm_ivec2_t *dest) {
+  dest->x = (a.x >= b.x) ? b.x : a.x;
+  dest->y = (a.y >= b.y) ? b.y : a.y;
+}
 
-int cm_ivec2_cross(cm_ivec2_t a, cm_ivec2_t b) { return a.x * b.y - a.y * b.x; }
+int64_t cm_ivec2_dot(const cm_ivec2_t a, const cm_ivec2_t b) {
+  return a.x * b.x + a.y * b.y;
+}
 
-int cm_ivec2_dist_squared(cm_ivec2_t a, cm_ivec2_t b) {
-  int xd = a.x - b.x;
-  int yd = a.y - b.y;
+int64_t cm_ivec2_cross(const cm_ivec2_t a, const cm_ivec2_t b) {
+  return a.x * b.y - a.y * b.x;
+}
+
+int64_t cm_ivec2_dist_squared(const cm_ivec2_t a, const cm_ivec2_t b) {
+  int64_t xd = a.x - b.x;
+  int64_t yd = a.y - b.y;
   return (xd * xd + yd * yd);
 }
 
-float cm_ivec2_dist(cm_ivec2_t a, cm_ivec2_t b) {
+float cm_ivec2_dist(const cm_ivec2_t a, const cm_ivec2_t b) {
   return sqrt((float)cm_ivec2_dist_squared(a, b));
 }
--- src/ivector/ivector2.h
+++ src/ivector/ivector2.h
@@ -1,24 +1,37 @@
 #ifndef CM_IVEC2_H
 #define CM_IVEC2_H
 
-typedef struct cm_ivec2_t cm_ivec2_t;
+#define CM_GET_ZERO_IVEC2 ((cm_ivec2_t){0, 0})
+#define CM_GET_E1_IVEC2 ((cm_ivec2_t){1, 0})
+#define CM_GET_E2_IVEC2 ((cm_ivec2_t){0, 1})
 
-void cm_ivec2_copy(cm_ivec2_t v, cm_ivec2_t dest);
-void cm_ivec2_zero(cm_ivec2_t v);
-void cm_ivec2_add(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest);
-void cm_ivec2_sub(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest);
+#include <stdint.h>
 
-void cm_ivec2_scale(cm_ivec2_t v, int s, cm_ivec2_t dest);
-void cm_ivec2_fill(cm_ivec2_t v, int val);
-void cm_ivec2_abs(cm_ivec2_t v, cm_ivec2_t dest);
-void cm_ivec2_max(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest);
+// TODO: SIMD optimization for module
+// TODO: support all int and float types(templates C++?)
+// TODO: min, max compare without if
 
-void cm_ivec2_min(cm_ivec2_t a, cm_ivec2_t b, cm_ivec2_t dest);
+typedef struct {
+  int64_t x, y;
+} cm_ivec2_t;
 
-int cm_ivec2_dot(cm_ivec2_t a, cm_ivec2_t b);
-int cm_ivec2_cross(cm_ivec2_t a, cm_ivec2_t b);
-int cm_ivec2_dist_squared(cm_ivec2_t a, cm_ivec2_t b);
+void cm_ivec2_init(cm_ivec2_t *v, int64_t x, int64_t y);
+void cm_ivec2_copy(const cm_ivec2_t v, cm_ivec2_t *dest);
+void cm_ivec2_zero(cm_ivec2_t *v);
+void cm_ivec2_add(const cm_ivec2_t x, const cm_ivec2_t y, cm_ivec2_t *dest);
+void cm_ivec2_sub(const cm_ivec2_t x, const cm_ivec2_t y, cm_ivec2_t *dest);
 
-float cm_ivec2_dist(cm_ivec2_t a, cm_ivec2_t b);
+void cm_ivec2_scale(const cm_ivec2_t v, int64_t s, cm_ivec2_t *dest);
+void cm_ivec2_fill(int64_t val, cm_ivec2_t *dest);
+void cm_ivec2_abs(const cm_ivec2_t v, cm_ivec2_t *dest);
+
+void cm_ivec2_max(const cm_ivec2_t x, const cm_ivec2_t y, cm_ivec2_t *dest);
+void cm_ivec2_min(const cm_ivec2_t x, const cm_ivec2_t y, cm_ivec2_t *dest);
+
+int64_t cm_ivec2_dot(const cm_ivec2_t x, const cm_ivec2_t y);
+int64_t cm_ivec2_cross(const cm_ivec2_t x, const cm_ivec2_t y);
+
+int64_t cm_ivec2_dist_squared(const cm_ivec2_t x, const cm_ivec2_t y);
+float cm_ivec2_dist(const cm_ivec2_t x, const cm_ivec2_t y);
 
 #endif
