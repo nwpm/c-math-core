521b31e25cfb6ab5b99588c3c532ce3ad17b42d5
nwpm <fwxgkt@gmail.com>
2026-02-22 11:20:49 +0300

    feat: utils add double_abs, double_max and double_is_equal

--- src/utils/cm_utils.h
+++ src/utils/cm_utils.h
@@ -1,22 +1,27 @@
 #ifndef CM_UTILS_H
 #define CM_UTILS_H
 
-#include "../../../include/cm_core/cm_types.h"
 #include <stdbool.h>
 
-#define CM_EPS 1.0e-20
+#define CM_EPS 1.0e-5
 
-static inline cm_real_t _cm_real_abs(cm_real_t x) {
-  return x >= 0 ? x : -1. * x;
+static inline double _cm_double_abs(double x) { return (x < 0. ? -x : x); }
+
+static inline double _cm_double_max(double x, double y) {
+  return (x < y ? y : x);
 }
+
+static inline bool _cm_double_is_equal(double x, double y) {
+  return _cm_double_abs(x - y) <
+         CM_EPS * _cm_double_max(_cm_double_abs(x), _cm_double_abs(y));
+}
+
+
 static inline long _cm_long_abs(long x) { return x >= 0 ? x : -1. * x; }
 static inline long long _cm_llong_abs(long long x) {
   return x >= 0 ? x : -1. * x;
 }
 static inline int _cm_abs(int x) { return x >= 0 ? x : -1. * x; }
 
-static inline bool _cm_real_equal(cm_real_t x, cm_real_t y) {
-  return _cm_real_abs(x - y) < CM_EPS;
-}
 
 #endif
